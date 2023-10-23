da5b0994fabb5eb551d58c73088789a57a291219
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-10-23 17:56:27 -0300

    Modelo de producto añadido

---
+++ src/models/product.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    default: "default.png",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ProductModel = mongoose.model("Product", ProductSchema, "products");
