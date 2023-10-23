7699308f59ca296d8d492d83045039cf71b9f118
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-10-23 19:33:16 -0300

    Rutas usuario añadidas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import {register} from "../controllers/user.controller.js";
+import {register, login, update} from "../controllers/user.controller.js";
+import { auth } from "../middlewares/auth.js";
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.put("/update", auth, update);
 
 export default router;
