ae9bafb2c0cb7943254fd4c60aaaaff256f61aae
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-10-24 13:39:42 -0300

    CRUD de casino finalizado

--- src/controllers/cafeteria.controller.js
+++ src/controllers/cafeteria.controller.js
@@ -22,4 +22,80 @@ async function add(req, res) {
   }
 }
 
-export { add };
+async function list(req, res) {
+  try {
+    const cafeterias = await CafeteriaModel.find();
+    return res.status(200).json({
+      status: "success",
+      cafeterias: cafeterias,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Ha ocurrido un error en la base de datos",
+    });
+  }
+}
+
+async function remove(req, res) {
+  try {
+    // Obtener el ID
+    const cafeteriaId = req.params.cafeteriaId;
+
+    // Buscar cafeteriao por ID
+    const cafeteria = await CafeteriaModel.find({
+      _id: cafeteriaId,
+    });
+
+    if (!cafeteria) {
+      return res.status(400).send({
+        status: "error",
+        message: "No se ha eliminado el casino",
+      });
+    }
+
+    await CafeteriaModel.findOneAndDelete(cafeteria);
+
+    return res.status(200).send({
+      status: "success",
+      message: "¡El casino ha sido eliminado correctamente!",
+      cafeteria: cafeteriaId,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ error: "Ha ocurrido un error en la base de datos" });
+  }
+}
+
+async function update(req, res) {
+  try {
+    const cafeteriaId = req.params.cafeteriaId;
+    const cafeteriaToUpdate = req.body;
+
+    // Buscar y actualizar
+    let cafeteriaUpdated = await CafeteriaModel.findByIdAndUpdate(
+      cafeteriaId,
+      cafeteriaToUpdate,
+      { new: true }
+    );
+
+    if (!cafeteriaUpdated) {
+      return res
+        .status(500)
+        .send({ error: "Ha ocurrido un error al actualizar" });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      message: "¡El cafeteriao se ha actualizado correctamente!",
+      cafeteriaUpdated,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ error: "Ha ocurrido un error en la base de datos" });
+  }
+}
+
+export { add, list, remove, update };
--- src/routes/cafeteria.routes.js
+++ src/routes/cafeteria.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { add } from "../controllers/cafeteria.controller.js";
+import { add, list, remove, update } from "../controllers/cafeteria.controller.js";
 import { auth } from "../middlewares/auth.js";
 
 const router = Router();
 
 router.post("/add", auth, add);
+router.get("/list", auth, list);
+router.delete("/remove/:cafeteriaId", auth, remove);
+router.put("/update/:cafeteriaId", auth, update);
 
 export default router;
