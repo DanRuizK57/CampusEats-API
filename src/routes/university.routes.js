f91d38d068f53d9ab54210e9763f927b7e605444
Claudio Saez <c.saez15@ufromail.cl>
2023-10-25 19:16:40 -0300

    se crean nuevas rutas - universidad

--- src/routes/university.routes.js
+++ src/routes/university.routes.js
@@ -1,5 +1,21 @@
 import { Router } from "express";
 import { list , create , update, remove } from "../controllers/university.controller.js";
+import multer from "multer";
+
+// Configuración de subida
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/universities/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "university-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploads = multer({ storage });
+
+
+
 
 const router = Router();
 
@@ -8,5 +24,8 @@ router.post("/create", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
 
+
+router.post("/upload-photo/:id", [auth, uploads.single("file0")], uploadPhoto);
+router.get("/photo/:file", auth, showPhoto);
 
 export default router;
